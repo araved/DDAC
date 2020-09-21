66032d9e9af4831bf371af547fed11b957392e7a
araved <hdkc93@gmail.com>
2020-09-21 08:54:01 +0800

    Presentation prep

--- WebApplication2/UploadCourse.aspx.cs
+++ WebApplication2/UploadCourse.aspx.cs
@@ -33,11 +33,11 @@ namespace WebApplication2
                 var container = new BlobContainerClient(connectionString, "epro-video-repo");
                 var file = file_upload.PostedFile;
                 var blob = container.GetBlobClient(file.FileName);
-                file.SaveAs(Server.MapPath("/temp/") + file.FileName); 
+                file.SaveAs(System.Web.Hosting.HostingEnvironment.MapPath("/temp") + file.FileName); 
                  using (Stream upload = File.OpenRead(Server.MapPath("temp/") + file.FileName))
                  {
                      blob.Upload(upload);
-                     File.Delete("/temp/"+file.FileName);
+                     File.Delete(System.Web.Hosting.HostingEnvironment.MapPath("/temp")+file.FileName);
                      uploadStatus.Text = "Upload Successful!";
                  }
 
